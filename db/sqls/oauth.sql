2820de22c5e61dcc1b2a4688a6ee0e4042e2941a
ivelasco <ivelasco@bindeo.com>
2016-03-29 12:27:19 +0200

    Public api full oauth

---
+++ db/sqls/oauth.sql
@@ -0,0 +1,16 @@
+# RESET
+
+/*
+DROP TABLE OAUTH_CLIENTS;
+DROP TABLE OAUTH_TOKENS;
+*/
+
+# CREATION
+
+# Tables
+
+source ../tables/OAUTH_CLIENTS.sql;
+source ../tables/OAUTH_TOKENS.sql;
+
+# Data
+source ../sqls/oauth_clients.sql;
