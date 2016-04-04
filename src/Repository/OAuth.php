59848fae5cf6396bd6de0117457f3b82b5c21578
ivelasco <ivelasco@bindeo.com>
2016-04-04 18:33:19 +0200

    Factum client and ip control

--- src/Repository/OAuth.php
+++ src/Repository/OAuth.php
@@ -20,12 +20,19 @@ class OAuth extends RepositoryAbstract
     public function oauthClient(OAuthClient $client)
     {
         $client->clean();
-        if (!$client->getName() or !$client->getSecret()) {
+        if ((!$client->getName() or !$client->getSecret()) and !$client->getIdClient()) {
             throw new \Exception(Exceptions::MISSING_FIELDS, 400);
         }
 
-        $sql = "SELECT ID_CLIENT, NAME, SECRET, ROLE FROM OAUTH_CLIENTS WHERE NAME = :name AND SECRET = :secret AND STATUS = 'A'";
-        $params = [':name' => mb_strtoupper($client->getName()), ':secret' => $client->getSecret()];
+        $sql = "SELECT ID_CLIENT, NAME, SECRET, ROLE, ALLOWED_IPS FROM OAUTH_CLIENTS WHERE STATUS = 'A' AND ";
+
+        if ($client->getIdClient()) {
+            $sql .= 'ID_CLIENT = :id';
+            $params = [':id' => $client->getIdClient()];
+        } else {
+            $sql .= 'NAME = :name AND SECRET = :secret';
+            $params = [':name' => mb_strtoupper($client->getName()), ':secret' => $client->getSecret()];
+        }
 
         $data = $this->db->query($sql, $params, 'Api\Entity\OAuthClient');
 
@@ -115,7 +122,7 @@ class OAuth extends RepositoryAbstract
         }
 
         // Get the token if it is active
-        $sql = 'SELECT TOKEN, EXPIRATION FROM OAUTH_TOKENS WHERE TOKEN = :token AND EXPIRATION > SYSDATE()';
+        $sql = 'SELECT TOKEN, EXPIRATION, FK_ID_CLIENT, FK_ID_USER FROM OAUTH_TOKENS WHERE TOKEN = :token AND EXPIRATION > SYSDATE()';
         $data = $this->db->query($sql, [':token' => $token->getToken()], 'Api\Entity\OAuthToken');
 
         if (!$data or $this->db->getError()) {
